6d298b89313910502502ed7c1a6081ddaa82e025
Robson Arruda <robsonarruda78820@gmail.com>
2025-12-31 16:32:36 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,19 @@
+window.revelar = ScrollReveal({
+    reset: true,
+    distance: '90px',
+    duration: 2000,
+    delay: 500
+});
+
+revelar.reveal('.efeito-txt-topo', { origin: 'bottom' });
+revelar.reveal('.efeito-img-topo', { origin: 'bottom' });
+// primeira seção
+
+revelar.reveal('.efeito-page2-txt-left', { origin: 'left' });
+revelar.reveal('.efeito-page2-txt-right', { origin: 'right' });
+// segunda seção
+
+revelar.reveal('.efeito-page3-top', { origin: 'top' });
+revelar.reveal('.efeito-page3-bottom', { origin: 'bottom' });
+// terceira seção
+
